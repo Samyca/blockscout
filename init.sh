9cb7d931b546da518b16ed8285e67042724492cb
darienmh <darienmh@gmail.com>
2024-07-20 13:11:34 -0500

    set(envs): config

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "COPY ENVS FILES"
+cp ./envs/example_common-blockscout.env ./envs/common-blockscout.env
+cp ./envs/example_common-db.env ./envs/common-db.env
+cp ./envs/example_common-frontend.env ./envs/common-frontend.env
+cp ./envs/example_common-smart-contract-verifier.env ./envs/common-smart-contract-verifier.env
+cp ./envs/example_common-stats.env ./envs/common-stats.env
+cp ./envs/example_common-user-ops-indexer.env ./envs/common-user-ops-indexer.env
+cp ./envs/example_common-visualizer.env ./envs/common-visualizer.env
+
+cp ./proxy/example_default.conf.template ./proxy/default.conf.template
+cp ./proxy/example_microservices.conf.template ./proxy/microservices.conf.template
